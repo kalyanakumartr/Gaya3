51ae87da5e33b7a8ad5ea4adbf3a815afe738f31
Ananth <kalyanakumartr@gmail.com>
2022-04-08 15:48:13 +0530

    Logo and Login
    Change-Id: Id791585c2a412091b13e8c8bfe415ef9cfa330aa

--- GayathriConstructions/src/main/java/org/hbs/gaya/security/WebSecurityConfig.java
+++ GayathriConstructions/src/main/java/org/hbs/gaya/security/WebSecurityConfig.java
@@ -1,13 +1,5 @@
 package org.hbs.gaya.security;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.hbs.gaya.controllers.LoginSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,18 +7,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
-
-	//@Autowired
-	//private LoginSuccessHandler	successHandler;
 
 	@Bean
 	public UserDetailsService userDetailsService()
